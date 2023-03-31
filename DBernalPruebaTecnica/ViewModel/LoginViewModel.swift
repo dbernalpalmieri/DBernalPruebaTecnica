880c3573dbc597a255754087e877db7ee1509b37
Daniel Bernal Palmieri <dbernalpalmieri@gmail.com>
2023-03-31 00:33:04 -0600

    Bluethood Works

--- DBernalPruebaTecnica/ViewModel/LoginViewModel.swift
+++ DBernalPruebaTecnica/ViewModel/LoginViewModel.swift
@@ -47,4 +47,48 @@ struct LoginViewModel{
             
         }
     }
+    func PostCifrado(_ usuario : Usuario, _ response : @escaping(Login?, Error?) -> Void){
+            util.urlComponents.scheme = "https"
+            util.urlComponents.host = "c3e09b62-462f-46bb-af52-ffa354627cca.mock.pstmn.io"
+            util.urlComponents.path = "/Encrypted"
+            
+            if let url = util.urlComponents.url {
+                
+                do{
+                    util.jsonEncoder.outputFormatting = .prettyPrinted
+                    
+                    let jsonBodyData = try util.jsonEncoder.encode(usuario)
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "POST"
+                    request.httpBody = jsonBodyData
+                    request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+                    
+                    util.urlSession.dataTask(with: request) { data, res, error in
+                        
+                        if let data = data, let httpResponse = res as? HTTPURLResponse, error == nil{
+                            if 200...400 ~=  httpResponse.statusCode {
+                                do{
+                                    let resData = try util.jsonDecoder.decode(Login.self, from: data)
+                                    response(resData, error)
+                                }catch let error{
+                                    response(nil, error)
+                                }
+                                
+                            }
+                        }else if let error = error{
+                            response(nil, error)
+                        }
+                        else{
+                            response(nil, nil)
+                        }
+                    }.resume()
+                    
+                    
+                }catch let error{
+                    response(nil, error)
+                }
+                
+                
+            }
+        }
 }
